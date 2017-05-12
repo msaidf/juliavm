c1680195db470df827859a86e51b9cc5656b9531
Pablo Margreff <pmargreff@gmail.com>
2017-05-12 19:47:39 -0300

    feature: adding platform option on install process

--- install.sh
+++ install.sh
@@ -19,7 +19,9 @@
     juliavm_create_directories
     juliavm_echo "Moving files ..."
     juliavm_copy_files
+    juliavm_set_platform
     juliavm_echo "Juliavm successfully installed!!"
+    exec bash
   }
 
   juliavm_create_directories(){
@@ -29,8 +31,31 @@
 
   juliavm_copy_files(){
     eval 'cp $CURRENT_DIR/juliavm.sh $INSTALL_DIR/juliavm'
-    echo "alias juliavm='$INSTALL_DIR/juliavm'" >> ~/.bashrc && exec bash
+    echo "alias juliavm='$INSTALL_DIR/juliavm'" >> ~/.bashrc
     eval 'cp -r $CURRENT_DIR/.git $INSTALL_DIR'
+  }
+
+  juliavm_set_platform(){
+    juliavm_echo "Choose a standard platform:"
+    juliavm_echo "  1 - osx 64 bits"
+    juliavm_echo "  2 - linux 64 bits"
+    juliavm_echo "  3 - linux 32 bits"
+    while [[ -z "$platform" || ("$platform" != 1 && "$platform" != 2 && "$platform" != 3) ]]
+    do
+      read platform
+      if [[ "$platform" != 1 && "$platform" != 2 && "$platform" != 3 ]]; then
+        juliavm_echo "Invalid option, try again!"
+      fi
+    done
+    if [[ "$platform" == 1 ]]; then
+      export JULIAVM_PLATFORM="-osx"
+    elif [[ "$platform" == 2 ]]; then
+      export JULIAVM_PLATFORM="-x64"
+    elif [[ "$platform" == 3 ]]; then
+      export JULIAVM_PLATFORM="-x86"
+    fi
+
+    juliavm_echo $JULIAVM_PLATFORM
   }
 
   juliavm_install
